1ea38d733e6f5ea9da8267b8a60bafacc336e826
Pim van den Broek <32633377+pimvandenbroek@users.noreply.github.com>
2021-11-03 16:37:10 +0100

    Sorted everything in functions, added logging

--- setup.sh
+++ setup.sh
@@ -1,45 +1,76 @@
 #!/bin/bash
 
-#----Check root privillege.
-rootperm(){
-  echo "Checking root"|date +"[%Y-%m-%d %H:%M:%S]"
-  if [ "$(id -u)" -ne 0 ]; then
-    error "You must be root"
-	exit 1
-  fi
-}
-rootperm
+echo 'Setup for wsl or not (y/n):'
+read wsl
 
-apt update -y > /dev/null 2>&1;
-echo "Installing zsh"|date +"[%Y-%m-%d %H:%M:%S]"
+sudo apt update -y > /dev/null 2>&1;
+
+#Check root privilege.
+rootperm(){
+    echo "Checking root"|date +"[%Y-%m-%d %H:%M:%S]"
+    if [ "$(id -u)" -ne 0 ]; then
+        error "You must be root"
+	    exit 1
+    fi    
+}
+
 #zsh
-apt install zsh -y > /dev/null 2>&1;
-chsh -s $(which zsh)
+zsh() {
+    echo "Installing zsh"|date +"[%Y-%m-%d %H:%M:%S]"
+    #zsh
+    apt install zsh -y > /dev/null 2>&1;
+}
 
 #spaceship
-echo "Installing spaceship-prompt"|date +"[%Y-%m-%d %H:%M:%S]"
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git --depth=1
-mkdir .zfunctions
-cd spaceship-prompt
-ln -sf "$PWD/spaceship.zsh" "/usr/local/share/zsh/site-functions/prompt_spaceship_setup"
-fpath=( "${ZDOTDIR:-$HOME}/.zfunctions" $fpath )
-cd spaceship-prompt
-ln -sf "$PWD/spaceship.zsh" "${ZDOTDIR:-$HOME}/.zfunctions/prompt_spaceship_setup"
+spaceship() {
+    echo "Installing spaceship-prompt"|date +"[%Y-%m-%d %H:%M:%S]"
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git --depth=1
+    mkdir .zfunctions
+    cd spaceship-prompt
+    ln -sf "$PWD/spaceship.zsh" "/usr/local/share/zsh/site-functions/prompt_spaceship_setup"
+    fpath=( "${ZDOTDIR:-$HOME}/.zfunctions" $fpath )
+    cd spaceship-prompt
+    ln -sf "$PWD/spaceship.zsh" "${ZDOTDIR:-$HOME}/.zfunctions/prompt_spaceship_setup"
+}
 
-echo "Installing autosuggestions"|date +"[%Y-%m-%d %H:%M:%S]"
 #zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+autosuggestions(){
+    echo "Installing autosuggestions"|date +"[%Y-%m-%d %H:%M:%S]"   
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+}
+
+wsl-ssh(){
+    #wsl2-ssh-pageant
+    destination="$HOME/.ssh/wsl2-ssh-pageant.exe"
+    wget -O "$destination" "https://github.com/BlackReloaded/wsl2-ssh-pageant/releases/latest/download/wsl2-ssh-pageant.exe"
+    # Set the executable bit.
+    chmod +x "$destination"   
+}
+
+#install default stuff
+install() {
+    which $1 &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Installing: ${1}..."|date +"[%Y-%m-%d %H:%M:%S]"   
+        apt install -y $1
+    else
+        echo "Already installed: ${1}"|date +"[%Y-%m-%d %H:%M:%S]"   
+    fi
+}
+
+#install everything
+rootperm
+zsh
+spaceship
+autosuggestions
+if [ $wsl = 'y' ]; then wsl-ssh; fi
+
+install python3-pip
+install jq
 
 #back to home
 cd
 
-if [ $2 = "wsl" ]; then 
-  #wsl2-ssh-pageant
-  destination="$HOME/.ssh/wsl2-ssh-pageant.exe"
-  wget -O "$destination" "https://github.com/BlackReloaded/wsl2-ssh-pageant/releases/latest/download/wsl2-ssh-pageant.exe"
-  # Set the executable bit.
-  chmod +x "$destination"
-fi
-
 #reload shell
+chsh -s $(which zsh)
 exec ${SHELL} -l
